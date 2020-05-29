21bfabb34e6904219a3af061fbb041aa1a2575a0
MackoZBogdanca <mackozbogdanca@yandex.com>
2020-05-29 13:29:17 +0200

    update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import StockTable from "./components/StockTable";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello</div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <StockTable />
+      </div>
+    </div>
+  );
+};
 
 export default App;
