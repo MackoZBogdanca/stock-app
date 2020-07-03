99729dab9b9aec54e49d31b8edf915b8b72b98f8
MackoZBogdanca <mackozbogdanca@yandex.com>
2020-07-03 17:52:45 +0200

    update App component to display StockTable component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import StockTable from "./components/StockTable";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 const App = () => {
