af67e2a63df594513fcf3ad6208c1ed93063ea14
MackoZBogdanca <mackozbogdanca@yandex.com>
2020-10-12 15:27:13 +0200

    add react-router-dom and update file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import StockTable from "./components/StockTable";
+import { Route } from "react-router-dom";
 
-// import "bootstrap/dist/css/bootstrap.min.css";
+import AppleDetails from "./components/companies/apple/AppleDetails";
+import GoogleDetails from "./components/companies/google/GoogleDetails";
+import MicrosoftDetails from "./components/companies/microsoft/MicrosoftDetails";
+import TeslaDetails from "./components/companies/tesla/TeslaDetails";
+
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <div className="container">
-        <StockTable />
+        <Route exact path="/" component={StockTable} />
+        <Route path="/appledetails" component={AppleDetails} />
+        <Route path="/googledetails" component={GoogleDetails} />
+        <Route path="/microsoftdetails" component={MicrosoftDetails} />
+        <Route path="/tesladetails" component={TeslaDetails} />
       </div>
     </div>
   );
